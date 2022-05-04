49fc2fe7a7cbc3da4c9b15fca3d6ad0f68908722
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-05-04 18:40:36 +0200

    edit 1

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const newBtn = document.querySelector('.btn--new');
+const holdBtn = document.querySelector('.btn--hold');
+const rollBtn = document.querySelector('.btn--roll');
+const dice = document.querySelector('.dice');
+const players = document.querySelectorAll('.player');
+
+rollBtn.addEventListener('click', () => {
+  const randomRoll = Math.floor(Math.random() * 6 + 1);
+  dice.setAttribute('src', `dice-${randomRoll}.png`);
+
+  if (randomRoll === 1) {
+    players.forEach(p => {
+      p.classList.toggle('player--active');
+    });
+    return;
+  }
+  const activePlayer = document.querySelector('.player--active');
+  const activePlayerScore = parseFloat(
+    activePlayer.querySelector('.current-score').innerText
+  );
+  activePlayer.querySelector('.current-score').innerText =
+    randomRoll + activePlayerScore;
+});
