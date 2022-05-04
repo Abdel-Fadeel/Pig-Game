a0e01f87de9f51592c0de2dd7724398b7f9fb340
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-05-04 18:56:17 +0200

    edit 2

--- script.js
+++ script.js
@@ -6,20 +6,45 @@ const rollBtn = document.querySelector('.btn--roll');
 const dice = document.querySelector('.dice');
 const players = document.querySelectorAll('.player');
 
+players.forEach(p => {
+  p.querySelector('.score').innerText = 0;
+  p.querySelector('.current-score').innerText = 0;
+});
+
 rollBtn.addEventListener('click', () => {
   const randomRoll = Math.floor(Math.random() * 6 + 1);
   dice.setAttribute('src', `dice-${randomRoll}.png`);
-
+  const activePlayer = document.querySelector('.player--active');
+  const activePlayerCurrentScore = parseFloat(
+    activePlayer.querySelector('.current-score').innerText
+  );
   if (randomRoll === 1) {
+    activePlayer.querySelector('.current-score').innerText = 0;
     players.forEach(p => {
       p.classList.toggle('player--active');
     });
+
     return;
   }
+
+  activePlayer.querySelector('.current-score').innerText =
+    randomRoll + activePlayerCurrentScore;
+});
+
+holdBtn.addEventListener('click', () => {
   const activePlayer = document.querySelector('.player--active');
+
   const activePlayerScore = parseFloat(
+    activePlayer.querySelector('.score').innerText
+  );
+  const activePlayerCurrentScore = parseFloat(
     activePlayer.querySelector('.current-score').innerText
   );
-  activePlayer.querySelector('.current-score').innerText =
-    randomRoll + activePlayerScore;
+  activePlayer.querySelector('.score').innerText =
+    activePlayerCurrentScore + activePlayerScore;
+  activePlayer.querySelector('.current-score').innerText = 0;
+
+  players.forEach(p => {
+    p.classList.toggle('player--active');
+  });
 });
